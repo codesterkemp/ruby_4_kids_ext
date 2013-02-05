513ca0fc122e2f2dd31613293d16d86d4fae5b31
codesterkemp <sparky_csk@yahoo.com>
2013-02-05 02:29:46 -0500

    added a score tracking feature to tutorial

---
+++ main.rb
@@ -0,0 +1,10 @@
+require 'gosu'
+require_relative 'window'
+require_relative 'player'
+require_relative 'bullet'
+require_relative 'laser'
+require_relative 'timer'
+#require_relative 'score'
+
+window = Window.new
+window.show
---
+++ score.rb
@@ -0,0 +1,9 @@
+# class Score
+#  	attr_reader 
+#  	def initialize
+#  		@score = 0
+#  	end
+#  	def update
+#  		@score += 1
+#  		puts @score
+#  	end
